777304e822418c0c9d7bc217a7a59e7ce140a0c6
xiaoymyl <445629858@qq.com>
2013-10-08 11:41:41 +0800

    must be good

---
+++ YZH/YZH.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+$(document).ready(function(){
+						
+
+						   
+						   
+						   
+						   $("#start").click(function(){
+							
+							$(".two").show();
+							$(".one").hide();
+							
+							})
+						   $(".aggre").click(function(){
+													  
+							$(".left_two").hide();
+							$(".left_three").show();
+													  
+						    });
+						   $(".submit_one").click(function(){
+							$(".left_three").hide();
+							$(".left_four").show();
+						   });
+						   $(".submit_two").click(function(){
+							$(".left_four").hide();
+							$(".left_five").show();
+						   });
+						   
+						   
+						   
+						   
+						   
+						   
+						   
+						   });
