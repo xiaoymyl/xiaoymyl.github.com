94238c2cd9cde1da580f21e3f342977c3b524019
xiaoymyl <445629858@qq.com>
2013-10-25 13:28:18 +0800

    sis

---
+++ html/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>首页</title>
+<link href="css/home.css" rel="stylesheet" type="text/css" />
+<script language="javascript" src="http://f2e.it/jq.js"></script>
+<script language="javascript" src="js/index.js"></script>
+</head>
+
+<body>
+<!--<?php include 'header.php'; ?>-->
+<div id="top_pic">
+  <div class="homepage_intro">
+    <div class="homepage_intro_player"> <a href="javascript:;" style="display: block;"> <img src="images/pic_1.jpg"></a> <a href="javascript:;" style="display: none;"> <img src="images/pic_2.jpg"></a> <a href="javascript:;" style="display: none;"> <img src="images/pic_3.jpg"></a> <a href="javascript:;" style="display: none;"> <img src="images/pic_4.jpg"></a> <a href="javascript:;" style="display: none;"> <img src="images/pic_5.jpg"></a> </div>
+    <div class="homepage_intro_player_control"> <a href="javascript:;" class="current"></a> <a href="javascript:;" class=""></a> <a href="javascript:;" class=""></a> <a href="javascript:;" class=""></a> <a href="javascript:;" class=""></a> </div>
+  </div>
+</div>
+<div class="content_tab">
+  <div id="work_share">
+    <div id="work_share_menu">
+      <ul>
+        <li id="menu_title"><a href="#"></a></li>
+        <li id="menu_food"><a href="#"></a></li>
+        <li id="menu_medicine"><a href="#"></a></li>
+        <li id="menu_it"><a href="#"></a></li>
+        <li id="menu_daily"><a href="#"></a></li>
+        <li id="menu_traffic"><a href="#"></a></li>
+      </ul>
+    </div>
+  </div>
+  <!--<div style="height:100px;width:100%"></div>-->
+    <div  id="menu_tab">
+        <ul>
+            <li>
+                <p id="pt"><a><i>大平台</i></a></p>
+                <img class="iteam" src="images/big.png" width="865" height="510"/>
+            </li>
+            <li>
+                <p id="data"><a><i>大数据</i></a></p>
+                <img class="iteam" src="images/data.png" width="865" height="510" />
+            </li>
+            <li>
+                <p id="zy"><a><i>大资源</i></a></p>
+                <img class="iteam" src="images/zy.png" width="865" height="510" />
+            </li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>
+
+<!--<?php include 'footer.php'; ?>-->
