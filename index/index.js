c8e23ad6a57af4f33ee8a4c6a86948352b4d310b
xiaoymyl <445629858@qq.com>
2013-11-11 14:06:51 +0800

    index

---
+++ index/index.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+$(document).ready(function(e){
+						   $(".button").click(function(){
+		                      
+                              $(".one").animate({left:'0px'});
+							  $(".two").animate({left:'100px'});
+							  $(".three").animate({left:'200px'});
+						   });
+						   });
+
+
+
+
+
